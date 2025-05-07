3fb0a3f97c3121dcea5b94fc84feea3877160b91
Azad Rashidov <azadlezgi@yandex.ru>
2025-05-07 19:16:38 +0400

    Update config_dist.php

--- config_dist.php
+++ config_dist.php
@@ -1,12 +1,12 @@
-<?php
-// database settings
-$db_host = "localhost";
-$db_user = "";
-$db_pass = "";
-$db_name = "";
-$db_prefix = "2cz11_";
-$db_charset = "utf8";
-
-define("DB_PREFIX", "2cz11_");
-define("COOKIE_PREFIX", "3HV9D_");
-?>
+if ($_SERVER['HTTP_HOST']=="100site.loc") {
+	// database settings
+	$db_host = "localhost";
+	$db_user = "root";
+	$db_pass = "";
+	$db_name = "100site";
+	$db_prefix = "c2y_";
+	define("DB_PREFIX", "c2y_");
+	define("COOKIE_PREFIX", "mz4_");
+	define("SITE", "site1");
+	define("ROBOTS", "Disallow");
+}
