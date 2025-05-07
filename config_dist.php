06c3cc236baedcc60d97d266c6cdc9099ab72186
Azad Rashidov <azadlezgi@yandex.ru>
2025-05-07 18:24:26 +0400

    Create config_dist.php

---
+++ config_dist.php
@@ -0,0 +1,12 @@
+<?php
+// database settings
+$db_host = "localhost";
+$db_user = "";
+$db_pass = "";
+$db_name = "";
+$db_prefix = "2cz11_";
+$db_charset = "utf8";
+
+define("DB_PREFIX", "2cz11_");
+define("COOKIE_PREFIX", "3HV9D_");
+?>
